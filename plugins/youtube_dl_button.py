b1ba55a73d83928692927b13038d0d7b44e0fd25
Justine72 <59611675+Justine72@users.noreply.github.com>
2020-01-08 21:59:05 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -189,6 +189,16 @@ async def youtube_dl_call_back(bot, update):
                 text=Translation.RCHD_TG_API_LIMIT.format(time_taken_for_download, humanbytes(file_size)),
                 message_id=update.message.message_id
             )
+        else:
+            is_w_f = False
+            images = await generate_screen_shots(
+                download_directory,
+                tmp_directory_for_each_user,
+                is_w_f,
+                Config.DEF_WATER_MARK_FILE,
+                300,
+                0
+            )
             logger.info(images)
             await bot.edit_message_text(
                 text=Translation.UPLOAD_START,
